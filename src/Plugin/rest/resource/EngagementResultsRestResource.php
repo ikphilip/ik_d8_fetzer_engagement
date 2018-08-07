d6f67680fe82fcca98696d594e69ecd2ee19cc6e
Philip Curley <philip@interactiveknowledge.com>
2018-08-07 12:32:09 -0400

    Support for form POST and storage

--- src/Plugin/rest/resource/EngagementResultsRestResource.php
+++ src/Plugin/rest/resource/EngagementResultsRestResource.php
@@ -6,9 +6,11 @@ use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
+use Drupal\ik_d8_fetzer_engagement\Entity\EngagementEntity;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * Provides a resource to get view modes by entity and bundle.
@@ -104,8 +106,8 @@ class EngagementResultsRestResource extends ResourceBase {
     /**
    * Responds to POST requests.
    *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The entity object.
+   * @param array $data
+   *   The POST request data.
    *
    * @return \Drupal\rest\ModifiedResourceResponse
    *   The HTTP response object.
@@ -114,17 +116,109 @@ class EngagementResultsRestResource extends ResourceBase {
    *   Throws exception expected.
    */
   public function post(array $data) {
-
-    \Drupal::logger('IK')->debug('REsults');
-    $entity = $data;
-
     // You must to implement the logic of your REST Resource here.
     // Use current user after pass authentication to validate access.
     if (!$this->currentUser->hasPermission('access content')) {
       throw new AccessDeniedHttpException();
     }
 
-    return new ModifiedResourceResponse($entity, 200);
+    if (empty($data)) {
+      throw new BadRequestHttpException('No POST data.');
+    }
+
+    // Engagement Entity ID required in order to POST data.
+    if (!isset($data['fid'])) {
+      throw new BadRequestHttpException('Bad request. Data missing.');
+    }
+
+    // Load the entity referenced by the POST.
+    $entity = EngagementEntity::load($data['fid']);
+
+    if (is_null($entity)) {
+      throw new BadRequestHttpException('Bad request. Data missing.');
+    }
+
+    $write = $this->writeResponse($data, $entity);
+
+    if ($write['error']) {
+      throw new BadRequestHttpException('Bad request. Data missing.');
+    }
+
+    return new ModifiedResourceResponse($write, 200);
+  }
+
+  /**
+   * Write the response to the database table.
+   * 
+   * @param array $data
+   * 
+   * @param Drupal\ik_d8_fetzer_engagement\Entity\EngagementEntity entity
+   *   EngagementEntity object.
+   * 
+   * @return array
+   */
+  public function writeResponse (array $data, EngagementEntity $entity) {
+    $fields = [
+      'fid' => (int) $entity->id(),
+      'uid' => (int) $this->currentUser->id(),
+      'time' => REQUEST_TIME,
+    ];
+
+    // Prepare data for INSERT.
+    switch ($entity->bundle()) {
+      case 'feedback':
+        $fields['feedback'] = $data['thoughts'] ?? NULL;
+        $fields['name'] = $data['name'] ?? NULL;
+        $fields['email'] = $data['email'] ?? NULL;
+        $fields['permission'] = (int) $data['permission'] ?? 0;
+
+        if (is_null($fields['feedback'])) {
+          $fields['error'] = 1;
+        }
+        break;
+      case 'poll':
+        $fields['poll_choices'] = $data['options'] ? serialize($data['options']) : NULL;
+        $fields['word'] = $data['other'] ?? NULL;
+
+        if (is_null($fields['poll_choices'])) {
+          $fields['error'] = 1;
+        }
+        break;
+      case 'words':
+        $fields['word'] = $data['word'] ?? NULL;
+        $fields['name'] = $data['name'] ?? NULL;
+        $fields['email'] = $data['email'] ?? NULL;
+
+        if (is_null($fields['word']) || empty($fields['word'])) {
+          $fields['error'] = 1;
+        }
+        break;
+      default:
+        $fields['error'] = 1;
+    }
+
+    // Check for any errors.
+    if ($fields['error']) {
+      return $fields;
+    }
+
+    // INSERT Data
+    $database = \Drupal::database();
+    $transaction = $database->startTransaction();
+    try {
+      $result = $database->insert('fetzer_engagement_responses')
+        ->fields($fields)
+        ->execute();
+
+      return ['rid' => (int) $result];
+    }
+    catch (Exception $e) {
+      $transaction->rollBack();
+
+      \Drupal::logger('IK Fetzer')->error($e->getMessage());
+
+      return ['error' => 1];
+    }
   }
 
 }
